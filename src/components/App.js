fd167e235f6ce0f89373cfe5e667fc5ba4e60919
Zack Tylor <taylor.zackory@gmail.com>
2017-10-19 13:09:12 -0500

    Update App do display Landing page component when on the root route.

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
+import Landing from './Landing';
+
 import './App.css';
 
 const App = () => (
   <BrowserRouter>
     <div className="app">
-      <Switch />
+      <Switch>
+        <Route exact path="/" component={Landing} />
+      </Switch>
     </div>
   </BrowserRouter>
 );
