e5f3f5e112239227c57bd7541bb1fedf360cd00d
Zack Tylor <taylor.zackory@gmail.com>
2017-10-19 17:48:14 -0500

    wrap app in Provider with store

--- src/components/App.js
+++ src/components/App.js
@@ -1,22 +1,32 @@
 import React from 'react';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import promise from 'redux-promise';
+import reducers from '../reducers';
 
-import Landing from './Landing';
 import NavBar from './NavBar';
+import Landing from './Landing';
+import PharmacyNew from './PharmacyNew';
 
 import './App.css';
 
+const createStoreWithMiddleware = applyMiddleware(promise)(createStore);
+
 const App = () => (
-  <BrowserRouter>
-    <div>
-      <NavBar />
-      <div className="container">
-        <Switch>
-          <Route exact path="/" component={Landing} />
-        </Switch>
+  <Provider store={createStoreWithMiddleware(reducers)}>
+    <BrowserRouter>
+      <div>
+        <NavBar />
+        <div className="container">
+          <Switch>
+            <Route exact path="/" component={Landing} />
+            <Route path="/pharmacy/new" component={PharmacyNew} />
+          </Switch>
+        </div>
       </div>
-    </div>
-  </BrowserRouter>
+    </BrowserRouter>
+  </Provider>
 );
 
 export default App;
