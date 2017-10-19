c5cfe9dd97c39acd5d321889439d849eebd7b3e7
Zack Tylor <taylor.zackory@gmail.com>
2017-10-19 16:11:41 -0500

    include navbar in app routing

--- src/components/App.js
+++ src/components/App.js
@@ -2,15 +2,19 @@ import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 import Landing from './Landing';
+import NavBar from './NavBar';
 
 import './App.css';
 
 const App = () => (
   <BrowserRouter>
-    <div className="app">
-      <Switch>
-        <Route exact path="/" component={Landing} />
-      </Switch>
+    <div>
+      <NavBar />
+      <div className="container">
+        <Switch>
+          <Route exact path="/" component={Landing} />
+        </Switch>
+      </div>
     </div>
   </BrowserRouter>
 );
