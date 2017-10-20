8468cdac8359607f84cd932b6d47fcdf51da24b3
Zack Tylor <taylor.zackory@gmail.com>
2017-10-20 10:41:29 -0500

    update create perscription action to no longer reciave a callback

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const ROOT_URL = 'http://httpbin.org/post ';
 
-export function createPerscription(values, callback) {
+export function createPerscription(values) {
   const request = axios.post(ROOT_URL, values);
   return {
     type: 'CREATE_POST',
