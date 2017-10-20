edcd0c2c524b5dfca9ea42f32fe30971ac926a44
Zack Tylor <taylor.zackory@gmail.com>
2017-10-20 15:25:23 -0500

    add pharmacies tate to combine reducers

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,9 +2,11 @@ import { combineReducers } from 'redux';
 import { reducer as formReducer } from 'redux-form';
 
 import perscriptions from './perscriptions';
+import pharmacies from './pharmacies';
 
 const rootReducer = combineReducers({
   perscriptions,
+  pharmacies,
   form: formReducer
 });
 
