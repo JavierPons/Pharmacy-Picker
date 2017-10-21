1b511e30e9c59f7b275b7d1d5ddb840b2642726c
Zack Tylor <taylor.zackory@gmail.com>
2017-10-20 22:28:33 -0500

    add divvydose image to landing component

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -9,6 +9,13 @@ const Landing = () => (
       <div className="header-content">
         <div className="header-content-inner">
           <h1>YOUR PERSCRIPTION YOUR CHOICE</h1>
+          <div className="text-center">
+            <img
+              className=""
+              src="https://stratosphere.digital/images/loader/efdb162ff0673107f04e1c68f1179cc2.png"
+              alt="pill drop"
+            />
+          </div>
           <hr />
           <p>Fill out our pharmacy selector form to get your perscriptions filled and shiped to a pharmacy near you.</p>
           <Link to="/pharmacy/new" className="btn btn-primary">
